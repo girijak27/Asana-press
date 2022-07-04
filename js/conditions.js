ce3fc8257630a2812c439c0bf4336bf4369805da
sakoyalkar <sai.koyalkar@copart.com>
2022-07-04 13:34:05 -0500

    loops

--- js/conditions.js
+++ js/conditions.js
@@ -26,3 +26,43 @@ if(x > y){
         alert("the greatest number is z: " + z);
     }    
 }
+
+
+// Write a program to find the greatest of three number.
+
+if(x > y && x > z){
+    console.log("x is the greatest number")
+} else if(y > z){
+    console.log('y is the greatest')
+} else {
+    console.log('z is the greatest')
+}
+
+// Write a program to find whether both numbers are equal.
+
+var x = 10;
+var y = 20;
+
+if(x == y){
+    console.log('they are equal');
+} else {
+    console.log('they are not equal')
+}
+
+if(x != y){
+    console.log('they are not equal');
+} else {
+    console.log('they are equal')
+}
+
+
+
+
+
+
+
+// Write a program to serve food in a hotel - Menu card.
+
+
+
+
---
+++ js/loops.js
@@ -0,0 +1,62 @@
+// There are three types of loops:
+
+// 1. for loop
+// 2. while loop
+// 3. do-while loop
+
+// iteration - repeat
+
+
+// for(initialization; condition, increment/decrement){
+
+// }
+
+// Ex: Write a program to print your name 10 times
+
+for(var i=0; i < 100; i=i+1){
+    console.log('My name is Girja');
+    console.log('My name is Goverdhan');
+}
+
+for(var i=100; i > 0; i=i-1){
+    console.log('My name is Girja');
+    console.log('My name is Goverdhan');
+}
+
+// Write a program to print 9 table.
+
+var num = 8;
+
+for(var i=1; i < 100; i=i+1){
+    console.log(num, ' * ', i, ' = ', num*i);
+}
+
+var i =0; // initialization
+while(condition){ // condition
+
+    console.log(num, ' * ', i, ' = ', num*i);
+
+   i = i+1; // increment/decrement
+}
+
+var i= 0;
+do{
+   console.log(i)
+
+ i++;
+}while(condition)
+
+console.log('Girija', 'Goverdhan', 'Sudarshan', 'Soni');
+
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+console.log('Girija');
+
+
+
