bb5c3362ffcd32c4a97131c07f193fa97bed848f
girijak27 <girija.k27@gmail.com>
2022-07-07 00:00:09 +0530

    js files

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+var name = "rice"
+alert(name);
+var flag = confirm('Are you sure?')
+alert(flag);
+name = prompt('Please order your plate ');
+alert(name);
+
+
+var arr = [ "poori","rice","dal","biryani","curry"];
+    console.log(arr);
