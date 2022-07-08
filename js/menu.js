691180d0429fd070ec7e00343902a2be1c678093
girijak27 <girija.k27@gmail.com>
2022-07-08 18:13:37 +0530

    loops

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,24 @@
-var name = "rice"
-alert(name);
-var flag = confirm('Are you sure?')
-alert(flag);
-name = prompt('Please order your plate ');
-alert(name);
 
+//var name = "rice"
+//alert(name);
+//var flag = confirm('Are you sure?')
+//alert(flag);
+//name = prompt('Please order your plate ');
+//alert(name);
+//
+// var arr = [ "poori","rice","dal","biryani","curry"];
+ //    console.log(arr);
 
-var arr = [ "poori","rice","dal","biryani","curry"];
-    console.log(arr);
+ //write a 8 table
+var mul = 8
+for( var i=1; i<10; i =i+1){
+    console.log(mul, '*',i, "=", mul*i);
+}
+// print fname
+var fname ="girija";
+console.log(fname);
+// print add two numbers
+var marks1 = 50;
+var marks2 =60;
+var add = marks1+marks2;
+console.log(add);
